9de92ddbf141e0c6ec9fd907a707495ca20b8d51
Duarte de Freitas <durtoff@gmail.com>
2019-07-18 20:45:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,11 +5,13 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <=19)
+  console.log("Your are a teenager");
 }
 
 function ageChecker(age) {
-
+  if (age <= 13 && age >=19)
+  console.log("Your are note a teenager");
 }
 
 function ternaryTeenager(age) {
